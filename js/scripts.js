0d3b7111437ae8cfab7fb392647eccbc3c17e891
Luke Philips and Tracie Weitzman <luke@>
2016-10-25 09:33:17 -0700

    add for loop

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,15 @@ $(document).ready(function() {
   flavors.forEach(function(flavor) {
     $('ul#icecream').append('<li>'+flavor+'</li>');
   });
+
+
+
+  var flavors = ["cookies and cream", "rocky road", "vanilla", "strawberry"];
+flavors.length;
+
+  for (var i = 0; i < flavors.length; i += 1){
+    $('ul#icecream').append('<li>'+flavors[i]+'</li>');
+  }
+
+
 });
